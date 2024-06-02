b2f2f49a4252d459d8681cfa46b393b6b5a3dee3
keisuke yoshida <82552894+suke-toudara@users.noreply.github.com>
2024-06-02 16:30:16 +0900

    Add files via upload

--- src/costmap_to_point_component.cpp
+++ src/costmap_to_point_component.cpp
@@ -45,34 +45,41 @@ void CostmapToPointComponent::gridmap_callback(const grid_map_msgs::msg::GridMap
 {
   grid_map::GridMap input_map;
   grid_map::GridMapRosConverter::fromMessage(*msg, input_map);
-  grid_map::Index current_pos(current_pose_.position.x, current_pose_.position.y);
+  // grid_map::Index current_pos(current_pose_.position.x, current_pose_.position.y);
+  grid_map::Index start(current_pose_.position.y, current_pose_.position.x);
   double search_radius_grid = serch_range_radius / input_map.getLength().x();
   const double resolution = 1.0 * M_PI / 180.0;
   const int num_ranges = static_cast<int>(2 * M_PI / resolution);
 
   auto ideal_laser_scan = sensor_msgs::msg::LaserScan();
-  ideal_laser_scan.header.stamp = this->now();
+  ideal_laser_scan.header.stamp = msg->header.stamp; //this->now();
   ideal_laser_scan.header.frame_id = "base_link";
   ideal_laser_scan.angle_min = 0.0;
   ideal_laser_scan.angle_max = 2 * M_PI;
   ideal_laser_scan.angle_increment = resolution;
   ideal_laser_scan.range_min = 0.0;
-  ideal_laser_scan.range_max = 100.0; //search_radius_grid * input_map.getLength().x();
+  ideal_laser_scan.range_max = std::numeric_limits<double>::max(); //search_radius_grid * input_map.getLength().x();
   ideal_laser_scan.ranges.resize(num_ranges);
-  ideal_laser_scan.intensities.resize(num_ranges, 100.0);
+  ideal_laser_scan.intensities.resize(num_ranges,100.0);
+  double angle = ideal_laser_scan.angle_min;
+
+  //TODO　円がgridmap全部に届いていないので探索範囲変更
+
   for (int i = 0; i < num_ranges; ++i) {
-    double angle = i * resolution;
-    grid_map::Index search_end(current_pos.x() + search_radius_grid * std::cos(angle),
-                                current_pos.y() + search_radius_grid * std::sin(angle));
-    double dist = 0.0;
-    for (grid_map::LineIterator iterator(input_map, current_pos, search_end); !iterator.isPastEnd(); ++iterator) {
-      if (input_map.at("combined", *iterator) <= 0.5) {
-      }else{
-        // dist = std::hypot((*iterator).x() - current_pos.x(), (*iterator).y() - current_pos.y())*resolution;
-        dist = std::hypot((*iterator).x()*input_map.getResolution(),(*iterator).y()*input_map.getResolution());
-        RCLCPP_INFO(get_logger(),"dist_x: %d",(*iterator).x());
-        RCLCPP_INFO(get_logger(),"dist_y: %d",(*iterator).y());
-        RCLCPP_INFO(get_logger(),"getResolution: %f",input_map.getResolution());
+    angle += ideal_laser_scan.angle_increment;
+    // grid_map::Index search_end(current_pos.x() + search_radius_grid * std::cos(angle),
+    //                             current_pos.y() + search_radius_grid * std::sin(angle));
+    grid_map::Index end(current_pose_.position.y + search_radius_grid * std::sin(angle),
+                        current_pose_.position.x + search_radius_grid * std::cos(angle));
+    double dist = INFINITY;
+    for (grid_map::LineIterator iterator(input_map,start,end); !iterator.isPastEnd(); ++iterator) {
+      if (input_map.at("combined", *iterator) >= 0.01) {
+        const grid_map::Index index(*iterator);
+        grid_map::Position pos;
+        input_map.getPosition(index, pos);
+        dist = std::hypot((pos.x()-start.x())*input_map.getResolution(),(pos.y()-start.y())*input_map.getResolution());
+        //dist = std::hypot(pos.x()*input_map.getResolution(),pos.y()*input_map.getResolution());
+        RCLCPP_INFO(get_logger(),"dist_x: %f",dist);
       }
     }
     ideal_laser_scan.ranges[i] = dist;
